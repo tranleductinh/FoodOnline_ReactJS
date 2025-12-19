6bacf396b808d200eb8ec2785939e7ef97386363
Tính Trần <tranleductinh@gmail.com>
2025-12-19 18:32:39 +0700

    feat: add PWA support with VitePWA plugin and include app icons
    - Updated package.json to include vite-plugin-pwa
    - Added PWA icons (192x192 and 512x512) to the public directory
    - Configured Vite to use VitePWA with auto-update and manifest settings

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate", // Tự động cập nhật khi có nội dung mới
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Food Order",
+        short_name: "FO",
+        description: "Siêu cấp đặc biệt",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png", // Bạn phải chuẩn bị icon này trong thư mục public
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png", // Bạn phải chuẩn bị icon này trong thư mục public
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     host: true,
-    port: 5173
+    port: 5173,
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  }
-})
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
